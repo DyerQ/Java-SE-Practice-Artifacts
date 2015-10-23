87fa2cbb7bcfca07b0b356157177604e211c1354
Ilya <ilyagukov@list.ru>
2015-10-23 21:32:33 +0300

    MailTemplate completed

--- Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailTemplate.java
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailTemplate.java
@@ -1,17 +1,26 @@
 package ru.ncedu.bestgroup.mailing;
 
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 
 public class MailTemplate {
     private String body;
-
+    public MailTemplate(String body){
+        this.body = body;
+    }
     public String getBody() {
         return body;
     }
 
-    public  Mail getMail(BusinessCard card, Properties defaultProperties){
-       return new Mail();
+    public Mail getMail(BusinessCard card, Properties defaultProperties) {
+
+
+        for (String s: defaultProperties.stringPropertyNames()){
+            String  replacement = defaultProperties.getProperty(card.getProperty(s));
+            body = body.replaceAll("#\\{"+ s +"\\}",replacement);
+        }
+        return new Mail(card.getMail(),body);
     }
 }
 
