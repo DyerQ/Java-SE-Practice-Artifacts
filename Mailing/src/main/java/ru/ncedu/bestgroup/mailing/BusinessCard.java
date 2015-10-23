9b538184344a3a0bec0933701fe8b52f0963e84c
DariaChernikova <darya.d.chernikova@gmail.com>
2015-10-23 21:56:33 +0300

    Business card created.

--- Mailing/src/main/java/ru/ncedu/bestgroup/mailing/BusinessCard.java
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/BusinessCard.java
@@ -8,6 +8,27 @@ public class BusinessCard {
     private String mail;
     private Map<String, String> properties = new HashMap<String, String>();
 
+    public BusinessCard(String mail, Map<String, String> properties) {
+        if (mail == null) {
+            throw new IllegalArgumentException("Mail is null!");
+        }
+        this.mail = mail;
+        this.properties = properties;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        return !(o == null || getClass() != o.getClass()) && mail.equals(((BusinessCard) o).getMail());
+
+    }
+
+    @Override
+    public int hashCode() {
+        return mail.hashCode();
+    }
+
     public String getMail() {
         return mail;
     }
