1e4dc1c3d22bbba3f4dd7e2a50e7a9244c455b1b
DariaChernikova <darya.d.chernikova@gmail.com>
2015-11-03 21:37:24 +0300

    mailproducer(refactoring)

--- Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailProducer.java
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailProducer.java
@@ -4,41 +4,28 @@ import java.util.*;
 import java.util.concurrent.BlockingQueue;
 
 public class MailProducer {
-    private String recipientsFileName;
     private BlockingQueue<Mail> queue;
     private Properties defaultProperties;
 
-    public MailProducer(String recipientsFileName, BlockingQueue<Mail> queue, Properties defaultProperties) {
-        this.recipientsFileName = recipientsFileName;
+    public MailProducer(BlockingQueue<Mail> queue, Properties defaultProperties) {
         this.queue = queue;
         this.defaultProperties = defaultProperties;
     }
 
-    public Collection<Mail> buildMail(Collection<MailTemplate> templates) throws Exception {
-        Set<BusinessCard> businesscards;
-        businesscards = XMLUtils.parseBusinessCards(XMLUtils.readFromFile(recipientsFileName));
+    public Collection<Mail> buildMail(Collection<MailTemplate> templates, Set<BusinessCard> businessCards) throws Exception {
         Collection<Mail> result = new ArrayList<Mail>();
-        if (businesscards == null){
-            throw new Exception("Recipients File does not exists");
-        }
-        else{
-            for (MailTemplate templ : templates) {
-                for (BusinessCard busincard : businesscards) {
-                    result.add(templ.getMail(busincard, defaultProperties));
-                }
+
+        for (MailTemplate template : templates) {
+            for (BusinessCard businessCard : businessCards) {
+                result.add(template.getMail(businessCard, defaultProperties));
             }
-            return result;
         }
+        return result;
     }
 
     public void enqueueMail(Collection<Mail> mails) throws Exception {
-        if (mails == null){
-            throw new Exception("Check your mail collection");
-        }
-        else{
-            for (Mail mail : mails) {
-                queue.add(mail);
-            }
+        for (Mail mail : mails) {
+            queue.add(mail);
         }
     }
 }
