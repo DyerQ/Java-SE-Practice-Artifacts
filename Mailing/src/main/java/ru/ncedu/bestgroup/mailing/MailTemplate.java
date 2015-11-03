4fafe42db9b142a91bdcaae299ecaec835bf2723
Ilya <ilyagukov@list.ru>
2015-11-03 20:23:47 +0300

    MailTemplateParser with directory

--- Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailTemplate.java
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/MailTemplate.java
@@ -1,30 +1,32 @@
 package ru.ncedu.bestgroup.mailing;
 
 import java.util.Properties;
-import java.util.regex.Pattern;
 
 
 public class MailTemplate {
     private String body;
     private String subject;
-    public MailTemplate(String body, String subject){
+
+    public MailTemplate(String body, String subject) {
         this.body = body;
         this.subject = subject;
     }
+
     public String getBody() {
         return body;
     }
-    public String getSubject(){
+
+    public String getSubject() {
         return subject;         //comment for commit
     }
 
     public Mail getMail(BusinessCard card, Properties defaultProperties) {
 
-        for (String s: defaultProperties.stringPropertyNames()){
-            String  replacement = defaultProperties.getProperty(card.getProperty(s));
-            body = body.replaceAll("#\\{"+ s +"\\}",replacement);
+        for (String s : defaultProperties.stringPropertyNames()) {
+            String replacement = defaultProperties.getProperty(card.getProperty(s));
+            body = body.replaceAll("#\\{" + s + "\\}", replacement);
         }
-        return new Mail(card.getMail(),body);
+        return new Mail(card.getMail(), body);
     }
 }
 
--- Mailing/src/main/java/ru/ncedu/bestgroup/mailing/parsing/MailTemplateParser.java
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/parsing/MailTemplateParser.java
@@ -2,25 +2,41 @@ package ru.ncedu.bestgroup.mailing.parsing;
 
 import ru.ncedu.bestgroup.mailing.MailTemplate;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.Callable;
 
 
 public class MailTemplateParser implements Callable<Collection<MailTemplate>> {
+    private String directory;
+
+    public MailTemplateParser(String directory) {
+        this.directory = directory;
+    }
+
     @Override
     public Collection<MailTemplate> call() throws Exception {
-        File mailTemplateFolder = new File("./mail-templates");
-
-        if (mailTemplateFolder.isDirectory()){
+        File mailTemplateFolder = new File(directory);
+        if (mailTemplateFolder.isDirectory()) {
             Collection<MailTemplate> result = new ArrayList<MailTemplate>();
-            for (File f: mailTemplateFolder.listFiles()){
-                result.add(new MailTemplate(bodyRead(f), f.getName()));
+            for (File f : mailTemplateFolder.listFiles()) {
+                try {
+                    result.add(new MailTemplate(bodyRead(f), f.getName()));
+                    f.renameTo(new File(directory + File.separator + "processed"));
+                } catch (IOException e) {
+                    f.renameTo(new File(directory + File.separator + "invalid"));
+                }
+                
+
             }
             return result;
-        }else throw new Exception("no such directory");
+        } else throw new Exception("no such directory");
     }
+
     private String bodyRead(File f) throws IOException {
         StringBuilder text = new StringBuilder();
         BufferedReader br = new BufferedReader(new FileReader(f));
