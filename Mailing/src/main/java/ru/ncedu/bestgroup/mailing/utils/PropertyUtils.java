5d157d56ea0b45d8e4e1f091e2213f639a961540
DariaChernikova <darya.d.chernikova@gmail.com>
2015-11-06 21:34:40 +0300

    PropertyUtils created.

---
+++ Mailing/src/main/java/ru/ncedu/bestgroup/mailing/utils/PropertyUtils.java
@@ -0,0 +1,45 @@
+package ru.ncedu.bestgroup.mailing.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyUtils {
+    private static final String DEFAULT_CONFIG_DIRECTORY = "config";
+    private static final String DEFAULT_APP_PROPERTIES_FILE_NAME = "application.properties";
+    private static final String DEFAULT_CARD_PROPERTIES_FILE_NAME = "default.properties";
+    private static final String DEFAULT_MAIL_SENDING_PROPERTIES_FILE_NAME = "mail-sending.properties";
+
+    public static Properties loadApplicationProperties() throws IOException{
+        return loadProperties(DEFAULT_APP_PROPERTIES_FILE_NAME);
+    }
+
+    public static Properties loadDefaulCardProperties() throws IOException{
+        return loadProperties(DEFAULT_CARD_PROPERTIES_FILE_NAME);
+    }
+
+    public static Properties loadMailSendingProperties() throws IOException{
+        return loadProperties(DEFAULT_MAIL_SENDING_PROPERTIES_FILE_NAME);
+    }
+
+    public static Properties loadNamedProperties(String fileName) throws IOException{
+        return loadProperties(fileName);
+
+    }
+
+    private static Properties loadProperties(String fileName){
+        Properties properties = new Properties();
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(new File(DEFAULT_CONFIG_DIRECTORY, fileName));
+            properties.load(in);
+            in.close();
+            return properties;
+        }catch (IOException e) {
+            return  properties;
+        }
+    }
+
+
+}
